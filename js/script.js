b00337a7eb6dc4b13c9d582680c14cb10b9ad020
analuz5 <anaclara.luz339@gmail.com>
2023-11-06 21:00:53 -0300

    Segundo passo

---
+++ js/script.js
@@ -0,0 +1,11 @@
+/**
+ * Obtém o CEP na API BrasilAPI
+ * @param {string} cep-
+ */
+
+
+
+document.getElementById('cidade').addEventListener 'submit', function(event) {
+    event.preventDefault(); // Isso evita a recarga da pagina
+    const cidade = document.getElementById('cidade').value
+}

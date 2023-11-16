305679832ae91c3cd388fb5dab8d4c82727a9c7c
analuz5 <anaclara.luz339@gmail.com>
2023-11-16 17:56:34 -0300

    casa

--- js/script.js
+++ js/script.js
@@ -1,32 +1,50 @@
 const container=document.querySelector('.container');
 const search=document.querySelector('.search-box button');
-const pesquisar=document.querySelector('.pesquisar');
-const detalhes=document.querySelector('.detalhes');
+const weatherBox=document.querySelector('.weather-box');
+const weatherDetails=document.querySelector('.weather-details');
+const erro=document.querySelector('.semLocalizacao');
 
 search.addEventListener('click', ()=> {
     const APIKey='98740ebc0d63bc0f8ba70090e5a091';
-    const cidade= document.querySelector('.pesquisar input').value;
+    const city= document.querySelector('.search-box input').value;
 
-    if (cidade=='')
+    if (city=='')
         return;
-    fetch(`https://api.openweathermap.org/data/2.5/weather?q=${cidade}&units=metric&appid=${APIKey}`).then(response=> response.json()).then(json=>{
-        const image=document.querySelector('.pesquisar img');
-        const temperatura=document.querySelector('pesquisar .temperatura .temperatura');
-        const descricao=document.querySelector('pesquisar .descricao');
-        const humidade=document.querySelector('detalhes .humidade span');
-        const vento=document.querySelector('.detalhes .vento span');
+    fetch(``).then(response=> response.json()).then(json=>{
+        const image=document.querySelector('.weather-box img');
+        const temperature=document.querySelector('weather-box .temperature');
+        const description=document.querySelector('weather-box .description');
+        const humidity=document.querySelector('weather-details .humidity span');
+        const wind=document.querySelector('weather-details .wind span');
 
         switch (json.weather[0].main){
-            case 'chuva':
-                image.src='';
+            case 'Clear':
+                image.src ='images/tempo/limpo.png';
             break;
-            case 'diaLimpo':
-                image.src= 'imagens/vetores/chuva.svg';
+            case 'Rain':
+                image.src ='images/tempo/chuva.png';
+            break;
+            case 'Snow':
+                image.src ='images/tempo/nevasca.png';
             break;
 
+            case 'Mist':
+                image.src = 'images/tempo/neblina.png';
+            break;
             default:
-                break;
+                image.src = 'images/tempo/nuvem';
         }
 
     })
 })
+if (!ce || cep.length < 8) {
+    resultadoCep.textContent = `É obrigatório informar um CEP!`;
+    return;
+} else {
+    resultadoCep.textContent = "";
+    const dadosCep = await obtemCep(cep);
+    if (dadosCep.erro) {
+        resultadoCep.textContent = "Erro ao consultar o CEP informado";
+        return;
+    }
+}
